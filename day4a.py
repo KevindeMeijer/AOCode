9b6edeaadb5c84e6724fb3de9f7cabf33059c96a
Kevin de Meijer <kevin.demeijer@student.hu.nl>
2019-12-04 14:13:16 +0100

    mistake correction
    error

--- day4a.py
+++ day4a.py
@@ -1,7 +1,7 @@
 ans = 0
 for pwd in range(171309, 643603+1):
     digits = [int(x) for x in str(pwd)]
-    print(pwd.digits)
+    print(pwd,digits)
     has_pair = any([digits[i] == digits[i+1] for i in range(len(digits)-1)])
     has_descend = any([digits[i] > digits[i+1] for i in range(len(digits)-1)])
     if has_pair and not has_descend:
