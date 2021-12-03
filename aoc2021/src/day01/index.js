4188aa68c9bd01f4b83f3b32ffa82175699bd738
Kevin de Meijer <kevin.demeijer@student.hu.nl>
2021-12-03 11:17:33 +0100

    Day01 and WORKING CODE RUNNER 🎉

---
+++ aoc2021/src/day01/index.js
@@ -0,0 +1,41 @@
+import run from "aocrunner";
+
+const parseInput = (rawInput) => rawInput.split('\n').map((n) => Number(n));
+
+const part1 = (rawInput) => {
+  const input = parseInput(rawInput);
+
+  const max = input.length -1;
+
+  let increases = 0;
+
+  for (let i = 0; i < max; i++) {
+    input[i + 1] > input[i] && increases++;
+  }
+
+  return increases;
+};
+
+const part2 = (rawInput) => {
+  const input = parseInput(rawInput);
+
+  const max = input.length -1;
+
+  let increases = 0;
+
+  for (let i = 0; i < max; i++) {
+    (input[i + 1] + input[i + 2] + input[i + 3]) > (input[i] + input[i + 1] + input[i + 2]) && increases++;
+  }
+
+  return increases;
+};
+
+run({
+  part1: {
+    solution: part1,
+  },
+  part2: {
+    solution: part2,
+  },
+  trimTestInputs: true,
+});
